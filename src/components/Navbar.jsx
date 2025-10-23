e9aab07aa7823588f7c0562637b44ca6068a506f
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2025-10-23 10:34:20 +0100

    Setup project and install reduxtoolkit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div></div>
+        <div>
+            <div>
+                <h2> MyShop </h2>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
